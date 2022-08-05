afe654a0b393d6c87b2dcc11f3abeea0fc77416a
wmchen <78456777@qq.com>
2022-08-05 17:34:47 +0800

    security

---
+++ arch_im/src/test/java/com/suyao/arch_im/ArchImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyao.arch_im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
